d703e4ba0d8531640633a66e0813ab850e433089
donnypm <donnypramudia04@gmail.com>
2021-09-15 18:18:42 +0700

    task-4-add-edit-delete-nextjs

--- components/formlist.js
+++ components/formlist.js
@@ -1,20 +1,48 @@
-import React from "react";
 import List from "./list";
+import React, { useState } from "react";
 
 const FormList = (props) => {
   const { formList, handleEdit, handleDelete } = props;
+  const [inputSearch, setInputSearch] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setInputSearch(e.target.value);
+  };
+
   return (
     <div>
-      {formList.map((list) => {
-        return (
-          <List
-            list={list}
-            handleEdit={handleEdit}
-            handleDelete={handleDelete}
-            key={list.id}
-          />
-        );
-      })}
+      <div className="search">
+        <input
+          name={inputSearch}
+          type="text"
+          placeholder="Search Title Here..."
+          onChange={handleChange}
+          value={inputSearch}
+          className="searchTerm"
+        />
+      </div>
+
+      {formList
+        .filter((list) => {
+          if (inputSearch === "") {
+            return list;
+          } else if (
+            list.title.toLowerCase().includes(inputSearch.toLowerCase())
+          ) {
+            return list;
+          }
+        })
+        .map((list) => {
+          return (
+            <List
+              list={list}
+              handleEdit={handleEdit}
+              handleDelete={handleDelete}
+              key={list.id}
+            />
+          );
+        })}
     </div>
   );
 };
